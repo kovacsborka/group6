586b4f74853eb2aba2b6c05d3d4af52778ef3249
kovacsborka <kovacsborbalaterez@gmail.com>
2022-01-26 16:54:13 +0100

    final changes

---
+++ frontend/script.js
@@ -0,0 +1,15 @@
+function revealImg1() {
+    document.getElementById("img1").src="Images/balazs.jpg"
+}
+
+function revealImg2() {
+    document.getElementById("img2").src="Images/gobi.jpg"
+}
+
+function revealImg3() {
+    document.getElementById("img3").src="Images/david.jpg"
+}
+
+function revealImg4() {
+    document.getElementById("img4").src="Images/rankiandras2.jpg"
+}
